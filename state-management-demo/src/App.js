0a68c043cc2882ce599de73bd83bce91e18b3133
sk-shahsi <sk.sashikumar055@gmail.com>
2026-01-08 20:08:07 +0530

    stateManagement

--- state-management-demo/src/App.js
+++ state-management-demo/src/App.js
@@ -1,6 +1,16 @@
+/*
+- import useState from react
+-call useState inside our function with an initial value
+- get back two thing in our array
+--use array destructurung to unpack those two things
+- use them to make our UI intractive
+
+cons[somthing, setSomthing] = useState(initialValue)
+
+
+*/
+
 import React from "react";
-
-
 function App(){
   console.log("App component rendered");
 
