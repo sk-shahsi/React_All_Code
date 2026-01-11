f9113376b422014346c02929168502f3b58a5d88
sk-shahsi <sk.sashikumar055@gmail.com>
2026-01-11 22:38:17 +0530

    movies -data added

---
+++ movie-database-interface/src/components/BollywoodMovies.js
@@ -0,0 +1,59 @@
+import React ,{useState} from 'react';
+import './BollywoodMovies.css';
+
+const bollywoodMovies =[
+    
+    {
+
+    id:1,
+    title:"Bade Miya chote Miya",
+    genre:"Action",
+    rating:8.9,
+    year:2025,
+    director:"David Dhawan",
+    cast:["Akshay Kumar","Salman Khan"],
+
+    },
+
+    {
+
+    id:2,
+    title:"Fighter 2",
+    rating:9.9,
+    year:2025,
+    director:"David Dhawan",
+    cast:["Akshay Kumar","Salman Khan"],
+
+    },
+    {
+
+    id:1,
+    title:"Bade Miya chote Miya",
+    rating:8.9,
+    year:2025,
+    director:"David Dhawan",
+    cast:["Akshay Kumar","Salman Khan"],
+
+    },
+    {
+
+    id:1,
+    title:"Bade Miya chote Miya",
+    rating:8.9,
+    year:2025,
+    director:"David Dhawan",
+    cast:["Akshay Kumar","Salman Khan"],
+
+    },
+    {
+
+    id:1,
+    title:"Bade Miya chote Miya",
+    rating:8.9,
+    year:2025,
+    director:"David Dhawan",
+    cast:["Akshay Kumar","Salman Khan"],
+
+    },
+
+]
