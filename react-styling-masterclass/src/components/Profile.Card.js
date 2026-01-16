32b676c5d56e739e42855116a5e5e04b1e7e1b1d
sk-shahsi <sk.sashikumar055@gmail.com>
2026-01-16 18:04:22 +0530

    set-up project css

--- react-styling-masterclass/src/components/Profile.Card.js
+++ react-styling-masterclass/src/components/Profile.Card.js
@@ -8,7 +8,7 @@ const ProfileCard = ()=>{
             <div className="{styles.cardHeader}">
 
                 <div className="{styles.profileImage}">
-                    <img src=""
+                    <img src="https://img.freepik.com/premium-vector/man-avatar-icon-flat-illustration-man-avatar-vector-icon-isolated-white-background_98396-5720.jpg?semt=ais_hybrid&w=740&q=80"
                     alt="Profile"/>
                 </div>
             </div>
