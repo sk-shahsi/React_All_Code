d4d4653eb198db119d2da71beed56f4e90e32e8d
sk-shahsi <sk.sashikumar055@gmail.com>
2026-01-16 16:43:01 +0530

    set-up project

--- react-styling-masterclass/src/App.js
+++ react-styling-masterclass/src/App.js
@@ -1,11 +1,20 @@
 import React from "react";
 import '/.App.css';
+import ProfileCard from "./components/Profile.Card";
 
 
 function App(){
 
   return(
-    <div className="App"></div>
+    <div className="App">
+      <header class="App-header">
+        <h1>React Styling Masterclass</h1>
+        <p>Building Beautyful component</p>
+      </header>
+      <main className="main-content"></main>
+      <ProfileCard/>
+
+    </div>
   )
 
 }
