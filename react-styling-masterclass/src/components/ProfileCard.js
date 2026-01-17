099c35ddd22b971404a8af7555840941f5c6e6eb
sk-shahsi <sk.sashikumar055@gmail.com>
2026-01-17 12:15:30 +0530

    set-up project css

---
+++ react-styling-masterclass/src/components/ProfileCard.js
@@ -0,0 +1,17 @@
+import React from  "react";
+import styled from 'styled-components';
+
+const CardContainer = styled.div`
+  max-width: 400px;
+  margin: 50px auto;
+  backgroung:white;
+  border-ardius:20px;
+  box-shadow: 0 20px 40px rgba(0,0,0,0.1);
+  overflow:hidden;
+  transition:0.3s ease, box-shadow 0.3s ease'
+  &:hover{
+  transform: translateY(-5px);
+  box-
+  }
+
+`;
